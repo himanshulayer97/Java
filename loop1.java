6f0d5f964e4da9228bbd1fdf092445e4ce9deebf
Himanshu Gaurav Toppo <101444203+himanshulayer97@users.noreply.github.com>
2022-11-02 12:01:58 +0530

    Add files via upload

---
+++ loop1.java
@@ -0,0 +1,11 @@
+package CoreJava;
+public class loop1 {
+public static void main(String[]args) {
+	int i=0;
+	while(i<5)//comparing the i value lesser than 5 :  0<5 1<5 2<5 3<5 4<5 5<5 (Six times execution)
+	{
+		i++;//1 2 3 4 5
+		System.out.println(i);//printing the value
+	}
+}
+}
